28c8ef350fa3a5f9435504c7a1ef824e870c4bc4
DiegoHessel <diego.verdi@sptech.school>
2026-01-30 17:01:35 -0300

    fix: corrigir erros de digitação nas funções de conversão de tipos

--- GO_PACKAGES/7-strconv.go
+++ GO_PACKAGES/7-strconv.go
@@ -6,7 +6,7 @@ import (
 )
 
 // convertendo string para inteiro
-func convercaoDeStringParaNumero() {
+func conversaoDeStringParaNumero() {
 	numeroStr := "123"
 
 	// strconv.Atoi = parseint. Então ele vai converter uma string para inteiro.
@@ -19,15 +19,30 @@ func convercaoDeStringParaNumero() {
 	fmt.Println("Número:", numero)
 }
 
-//converção de inteiro para string
-func convercaoDeNumeroParaString(){
+// converção de inteiro para string
+func conversaoDeNumeroParaString() {
 	numero2 := 4566
 	// O método strconv.Itoa converte um número inteiro em uma string.
 	// Ele é útil para transformar valores numéricos em texto para exibição ou manipulação.
 	numeroStr2 := strconv.Itoa(numero2)
-	fmt.Println("Número:",numeroStr2 )
+	fmt.Println("Número:", numeroStr2)
 }
+
+// O método strconv.ParseFloat converte uma string que representa um número decimal em um valor float64.
+// Ele é útil para interpretar números decimais armazenados como texto. Caso a string não seja válida, retorna um erro.
+func conversaoDeStringParaFoat() {
+	floatStr := "12.9"
+
+	valorFloat, err := strconv.ParseFloat(floatStr, 64)
+	if err != nil {
+		fmt.Println("Erro: ", err)
+		return
+	}
+	fmt.Println("Número:", valorFloat)
+}
+
 func main() {
-	convercaoDeNumeroParaString()
-	convercaoDeStringParaNumero()
+	conversaoDeNumeroParaString()
+	conversaoDeStringParaNumero()
+	conversaoDeStringParaFoat()
 }
